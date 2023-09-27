2bcd41cf5481991f26836fa68297ccee48efb8a8
Ian <iancraine09@gmail.com>
2023-09-27 01:15:46 -0500

    added gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio';
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    this.animation();
+  }
+
+  public animation(){
+    let elements = document.querySelectorAll('.animated-ele');
+    elements.forEach((ele) => {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.animated-ele',
+          start: 'center center',
+          end: 'bottom center',
+          scrub: true,
+          markers: true,
+        }
+      });
+      tl.to('.animated-ele',{
+        opacity: 0,
+        // duration: 5
+      })
+    })
+    
+  }
 }
